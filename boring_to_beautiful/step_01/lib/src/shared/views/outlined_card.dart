732e2e555cc53bbe221d7e8597992883451a28e7
laurac8r <itslaurakaminskiy@gmail.com>
2022-12-17 20:34:11 -0500

    added motion and animation

--- boring_to_beautiful/step_01/lib/src/shared/views/outlined_card.dart
+++ boring_to_beautiful/step_01/lib/src/shared/views/outlined_card.dart
@@ -2,11 +2,10 @@ import 'package:flutter/material.dart';
 
 class OutlinedCard extends StatefulWidget {
   const OutlinedCard({
-    super.key,
+    Key? key,
     required this.child,
     this.clickable = true,
-  });
-
+  }) : super(key: key);
   final Widget child;
   final bool clickable;
 
@@ -15,22 +14,57 @@ class OutlinedCard extends StatefulWidget {
 }
 
 class _OutlinedCardState extends State<OutlinedCard> {
+  bool _hovered = false;
+
   @override
   Widget build(BuildContext context) {
+    final borderRadius = BorderRadius.circular(_hovered ? 20 : 8);
+    const animationCurve = Curves.easeInOut;
     return MouseRegion(
+      onEnter: (_) {
+        if (!widget.clickable) return;
+        setState(() {
+          _hovered = true;
+        });
+      },
+      onExit: (_) {
+        if (!widget.clickable) return;
+        setState(() {
+          _hovered = false;
+        });
+      },
       cursor: widget.clickable
           ? SystemMouseCursors.click
           : SystemMouseCursors.basic,
-      child: Container(
+      child: AnimatedContainer(
+        duration: kThemeAnimationDuration,
+        curve: animationCurve,
         // Add box decoration here
         decoration: BoxDecoration(
           border: Border.all(
             color: Theme.of(context).colorScheme.outline,
             width: 1,
-          )
+          ),
+          borderRadius: borderRadius
+        ),
+        foregroundDecoration: BoxDecoration(
+          color: Theme.of(context).colorScheme.onSurface.withOpacity(
+            _hovered ? 0.12 : 0
+          ),
+          borderRadius: borderRadius
         ),
         // Add box decoration here
-        child: widget.child,
+        child: TweenAnimationBuilder<BorderRadius>(
+          duration: kThemeAnimationDuration,
+          curve: animationCurve,
+          tween: Tween(begin: BorderRadius.zero, end: borderRadius),
+          builder: (context, borderRadius, child) => ClipRRect(
+            clipBehavior: Clip.antiAlias,
+            borderRadius: borderRadius,
+            child: child
+          ),
+          child: widget.child,
+        ),
       ),
     );
   }
