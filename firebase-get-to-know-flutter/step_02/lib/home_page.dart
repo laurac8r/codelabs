4c55aa29ffbb2d1e7e1247dc8174b706acb02d57
laurac8r <itslaurakaminskiy@gmail.com>
2022-12-26 20:13:10 -0500

    added RSVP button and auth flow

--- firebase-get-to-know-flutter/step_02/lib/home_page.dart
+++ firebase-get-to-know-flutter/step_02/lib/home_page.dart
@@ -2,8 +2,12 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
+import 'main.dart';
+import 'src/authentication.dart';
 import 'src/widgets.dart';
 
 class HomePage extends StatelessWidget {
@@ -11,29 +15,37 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Firebase Meetup'),
-      ),
-      body: ListView(
-        children: <Widget>[
-          Image.asset('assets/codelab.png'),
-          const SizedBox(height: 8),
-          const IconAndDetail(Icons.calendar_today, 'October 30'),
-          const IconAndDetail(Icons.location_city, 'San Francisco'),
-          const Divider(
-            height: 8,
-            thickness: 1,
-            indent: 8,
-            endIndent: 8,
-            color: Colors.grey,
-          ),
-          const Header("What we'll be doing"),
-          const Paragraph(
-            'Join us for a day full of Firebase Workshops and Pizza!',
-          ),
-        ],
-      ),
-    );
+    return Consumer<ApplicationState>(
+        builder: (context, appState, child) => Scaffold(
+              appBar: AppBar(
+                title: const Text('Firebase Meetup'),
+              ),
+              body: ListView(
+                children: <Widget>[
+                  Image.asset('assets/codelab.png'),
+                  const SizedBox(height: 8),
+                  const IconAndDetail(Icons.calendar_today, 'October 30'),
+                  const IconAndDetail(Icons.location_city, 'San Francisco'),
+                  Consumer<ApplicationState>(
+                    builder: (context, appState, _) => AuthFunc(
+                        loggedIn: appState.loggedIn,
+                        signOut: () {
+                          FirebaseAuth.instance.signOut();
+                        }),
+                  ),
+                  const Divider(
+                    height: 8,
+                    thickness: 1,
+                    indent: 8,
+                    endIndent: 8,
+                    color: Colors.grey,
+                  ),
+                  const Header("What we'll be doing"),
+                  const Paragraph(
+                    'Join us for a day full of Firebase Workshops and Pizza!',
+                  ),
+                ],
+              ),
+            ));
   }
 }
